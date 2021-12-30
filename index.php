167910e827fee3e83ea26ecb746c90e98ff7bda6
Shakib Farzan <farzan.shakib@gmail.com>
2021-12-30 12:43:26 +0330

    added favicon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="favicon.ico">
     <link rel="stylesheet" href="styles.css">
     <title>Calculator</title>
 </head>
@@ -14,8 +15,13 @@
         <?php
         if (isset($_GET['calculateBtn'])) {
             $input = $_GET['calculateInput'];
-            $res = eval('return ' . $input . ';');
-            echo "<p>Result is $res</p>";
+            $input = str_replace(' ', '', $input);
+            if (preg_match('[([-+/*]\d+(\d+)?)]', $input)) {
+                $res = eval('return ' . $input . ';');
+                echo "<p>Result is $res</p>";
+            } else {
+                echo "<p style=color:red>Please insert math expression!</p>";
+            }
             echo "<a href=<?php include('form.php');?>Back</a>";
         } else {
             include('form.php');
