5428128c1cbc5f856492f42041d03d452fe43143
Shakib Farzan <farzan.shakib@gmail.com>
2021-12-30 10:17:45 +0330

    created index php file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Calculator</title>
+</head>
+
+<body>
+    <form>
+        <input type="text" name="calculateInput" id="calculateInput" placeholder="Enter your expression...">
+        <button type="submit" name="calculateBtn" id="claculateBtn">Calculate</button>
+    </form>
+</body>
+
+</html>
